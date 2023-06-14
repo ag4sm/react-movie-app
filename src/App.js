51d0fd9f56e824a6ecffbb60dd86c91db87e2d55
ag4sm <56977525+ag4sm@users.noreply.github.com>
2023-06-14 14:33:48 -0400

    setup API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import {useEffect} from 'react';
 
 const API_URL = 'http://www.omdbapi.com?apikey=73830b8e';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    };
+    
+    useEffect(() => {
+        searchMovies('Spiderman')
+    }, []); 
+
     return(
         <h1>Hello World!</h1>
     );
